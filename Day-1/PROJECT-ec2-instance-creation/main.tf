0a615c631219af47bf595ddd081b868980fca4df
Ranjith-cloud-code <149606380+Ranjith-cloud-code@users.noreply.github.com>
2025-01-26 20:58:42 +0000

    New commmit

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -1,8 +1,12 @@
 provider "aws" {
-    region = "us-east-1"  # Set your desired AWS region
+    region = "eu-central-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+    ami           = "ami-0e320147c22e46f12"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
+    tags = {
+    Name = "tf-example"
+  }
+
 }
